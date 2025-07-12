84fabcc50615e4b272a9258e4cf39ade7bdee29b
tkaixiang <tkaixiang@gmail.com>
2025-07-12 20:50:56 +0800

    Improve image loading so that it reduces the number of double requests

--- src/muya/lib/parser/render/renderInlines/loadImageAsync.js
+++ src/muya/lib/parser/render/renderInlines/loadImageAsync.js
@@ -1,9 +1,25 @@
-import { getUniqueId, loadImage } from '../../../utils'
+import { getUniqueId } from '../../../utils'
 import { insertAfter, operateClassName } from '../../../utils/domManipulate'
 import { CLASS_OR_ID } from '../../../config'
 
+const addImageToContainer = (imageText, img, className) => {
+  if (imageText.classList.contains('ag-inline-image')) {
+    const imageContainer = imageText.querySelector('.ag-image-container')
+    const oldImage = imageContainer.querySelector('img')
+    if (oldImage) {
+      oldImage.remove()
+    }
+    imageContainer.appendChild(img)
+    imageText.classList.remove('ag-image-loading')
+    imageText.classList.add('ag-image-success')
+  } else {
+    insertAfter(img, imageText)
+    operateClassName(imageText, 'add', className)
+  }
+}
+
 export default function loadImageAsync(imageInfo, attrs, className, imageClass) {
-  const { src, isUnknownType } = imageInfo
+  const { src } = imageInfo
   let id
   let isSuccess
   let w
@@ -21,77 +37,79 @@ export default function loadImageAsync(imageInfo, attrs, className, imageClass) 
     reload = true
   }
   if (reload) {
+    let addedToImageContainer = false
     id = getUniqueId()
-    loadImage(src, isUnknownType)
-      .then(({ url, width, height }) => {
-        const imageText = document.querySelector(`#${id}`)
-        const img = document.createElement('img')
-        let dispMsec = Date.now()
-        let touchMsec = dispMsec
-        if (/^file:\/\//.test(src)) {
-          domsrc = url + '?msec=' + dispMsec
-        } else {
-          domsrc = url
-        }
-        img.src = domsrc
-        if (attrs.alt) img.alt = attrs.alt.replace(/[`*{}[\]()#+\-.!_>~:|<>$]/g, '')
-        if (attrs.title) img.setAttribute('title', attrs.title)
-        if (attrs.width && typeof attrs.width === 'number') {
-          img.setAttribute('width', attrs.width)
-        }
-        if (attrs.height && typeof attrs.height === 'number') {
-          img.setAttribute('height', attrs.height)
-        }
-        if (imageClass) {
-          img.classList.add(imageClass)
-        }
 
-        if (imageText) {
-          if (imageText.classList.contains('ag-inline-image')) {
-            const imageContainer = imageText.querySelector('.ag-image-container')
-            const oldImage = imageContainer.querySelector('img')
-            if (oldImage) {
-              oldImage.remove()
-            }
-            imageContainer.appendChild(img)
-            imageText.classList.remove('ag-image-loading')
-            imageText.classList.add('ag-image-success')
-          } else {
-            insertAfter(img, imageText)
-            operateClassName(imageText, 'add', className)
-          }
-        }
-        if (this.urlMap.has(src)) {
-          this.urlMap.delete(src)
-        }
-        this.loadImageMap.set(src, {
-          id,
-          isSuccess: true,
-          width,
-          height,
-          dispMsec,
-          touchMsec,
-          domsrc
-        })
+    const img = document.createElement('img')
+    let dispMsec = Date.now()
+    let touchMsec = dispMsec
+    if (/^file:\/\//.test(src)) {
+      domsrc = src + '?msec=' + dispMsec
+    } else {
+      domsrc = src
+    }
+    img.src = domsrc
+    if (attrs.alt) img.alt = attrs.alt.replace(/[`*{}[\]()#+\-.!_>~:|<>$]/g, '')
+    if (attrs.title) img.setAttribute('title', attrs.title)
+    if (attrs.width && typeof attrs.width === 'number') {
+      img.setAttribute('width', attrs.width)
+    }
+    if (attrs.height && typeof attrs.height === 'number') {
+      img.setAttribute('height', attrs.height)
+    }
+    if (imageClass) {
+      img.classList.add(imageClass)
+    }
+
+    if (this.urlMap.has(src)) {
+      this.urlMap.delete(src)
+    }
+
+    const imageText = document.querySelector(`#${id}`) // imageText refers to the image container
+    if (imageText) {
+      addImageToContainer(imageText, img, className)
+      addedToImageContainer = true
+    }
+
+    img.onload = () => {
+      const imageText = document.querySelector(`#${id}`) // imageText refers to the image container
+
+      if (imageText && !addedToImageContainer) {
+        addImageToContainer(imageText, img, className)
+        addedToImageContainer = true
+      }
+
+      this.loadImageMap.set(src, {
+        id,
+        isSuccess: true,
+        img: img,
+        width: img.naturalWidth,
+        height: img.naturalHeight,
+        dispMsec,
+        touchMsec,
+        domsrc,
+        addedToImageContainer
       })
-      .catch(() => {
-        const imageText = document.querySelector(`#${id}`)
-        if (imageText) {
-          operateClassName(imageText, 'remove', CLASS_OR_ID.AG_IMAGE_LOADING)
-          operateClassName(imageText, 'add', CLASS_OR_ID.AG_IMAGE_FAIL)
-          const image = imageText.querySelector('img')
-          if (image) {
-            image.remove()
-          }
+    }
+    img.onerror = () => {
+      const imageText = document.querySelector(`#${id}`)
+      if (imageText) {
+        operateClassName(imageText, 'remove', CLASS_OR_ID.AG_IMAGE_LOADING)
+        operateClassName(imageText, 'add', CLASS_OR_ID.AG_IMAGE_FAIL)
+        const image = imageText.querySelector('img')
+        if (image) {
+          image.remove()
         }
-        if (this.urlMap.has(src)) {
-          this.urlMap.delete(src)
-        }
-        this.loadImageMap.set(src, {
-          id,
-          isSuccess: false
-        })
+      }
+      if (this.urlMap.has(src)) {
+        this.urlMap.delete(src)
+      }
+      this.loadImageMap.set(src, {
+        id,
+        isSuccess: false,
+        addedToImageContainer: false
       })
+    }
   } else {
     const imageInfo = this.loadImageMap.get(src)
 
@@ -100,6 +118,16 @@ export default function loadImageAsync(imageInfo, attrs, className, imageClass) 
     w = imageInfo.width
     h = imageInfo.height
     domsrc = imageInfo.domsrc
+    if (!imageInfo.addedToImageContainer) {
+      const imageText = document.querySelector(`#${id}`)
+      if (imageText) {
+        addImageToContainer(imageText, imageInfo.img, className)
+        this.loadImageMap.set(src, {
+          ...imageInfo,
+          addedToImageContainer: true
+        })
+      }
+    }
   }
 
   return { id, isSuccess, domsrc, width: w, height: h }
