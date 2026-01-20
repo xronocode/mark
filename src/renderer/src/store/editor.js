edc7b5be0e82f13a379882c900771f5e2c4d30f2
tkaixiang <tkaixiang@gmail.com>
2026-01-20 15:16:40 +0800

    Fix bug with saving system not updating lastSavedId

--- src/renderer/src/store/editor.js
+++ src/renderer/src/store/editor.js
@@ -349,6 +349,7 @@ export const useEditorStore = defineStore('editor', {
       window.electron.ipcRenderer.on('mt::tab-saved', (_, tabId) => {
         const tab = this.tabs.find((f) => f.id === tabId)
         if (tab) {
+          tab.lastSavedHistoryId = tab.history.stack[tab.history.lastEditIndex].id
           tab.isSaved = true
         }
       })
